0e34924c2fe31f240c785459a62038e69bfb31e2
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-26 19:53:21 -0300

    add spring project

---
+++ src/main/java/org/example/gamesapi/GamesApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesApiApplication.class, args);
+    }
+
+}
