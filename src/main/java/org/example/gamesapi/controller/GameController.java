ebc98a4c11313bbab4c91bc6af5403c3ec331949
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-27 22:25:09 -0300

    feat: add return in controller

---
+++ src/main/java/org/example/gamesapi/controller/GameController.java
@@ -0,0 +1,25 @@
+package org.example.gamesapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.gamesapi.DTO.GameDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.example.gamesapi.service.GameService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+@AllArgsConstructor
+public class GameController {
+
+    private final GameService gameService;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<GameDTO>> getAllGames() {
+    List<GameDTO> games = gameService.getGames();
+    return ResponseEntity.ok().body(games);
+    }
+}
