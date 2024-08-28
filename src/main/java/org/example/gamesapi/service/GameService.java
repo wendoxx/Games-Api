3ff3877ae957a31ef4f845373eebc24a87b60573
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-27 22:22:22 -0300

    feat: add logic in GameService

---
+++ src/main/java/org/example/gamesapi/service/GameService.java
@@ -0,0 +1,18 @@
+package org.example.gamesapi.service;
+
+import lombok.AllArgsConstructor;
+import org.example.gamesapi.feign.GameClient;
+import org.springframework.stereotype.Service;
+import org.example.gamesapi.DTO.GameDTO;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class GameService {
+    private final GameClient gameClient;
+
+    public List<GameDTO> getGames(){
+        return gameClient.getGames();
+    }
+}
