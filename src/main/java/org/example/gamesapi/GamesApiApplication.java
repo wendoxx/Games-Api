f6b8d8fd7f3c79a1134a20fb9e198f383814d3ad
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-27 22:20:07 -0300

    feat: add @EnableFeignClient annotation

--- src/main/java/org/example/gamesapi/GamesApiApplication.java
+++ src/main/java/org/example/gamesapi/GamesApiApplication.java
@@ -2,8 +2,10 @@ package org.example.gamesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GamesApiApplication {
 
     public static void main(String[] args) {
